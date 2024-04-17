7071eec40d31fbf8e823360f3e15f9791cfb31e1
Yon <mariaymartin71@gmail.com>
2024-04-17 15:00:13 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <header>
+          <div className=''>
+            <a href="">YonAlva</a>
+            <ul className='barra'>
+              <li>home</li>
+              <li>portafolio</li>
+              <li>contact</li>
+            </ul>
+          </div>
+        </header>
+        <div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
